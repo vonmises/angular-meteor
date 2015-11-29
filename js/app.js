62d493943501194071a9f4ed97f23490209b9512
vonmises <vonmises@users.noreply.github.com>
2015-11-29 23:08:49 +0300

    binding the "parties" array to a mongo collection

--- js/app.js
+++ js/app.js
@@ -1,20 +1,8 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient){
     angular.module("socially", ["angular-meteor"])
-        .controller(
-        "PartiesListCtrl", function($scope){
-        $scope.parties = [
-            {
-                name: "dubstep-free zone",
-                description: "can we please just for an evening not listen to dubstep"
-            },
-            {
-                name: "all dubstep all the time",
-                description: "get it on!"
-            },
-            {
-                name: "savage lounging",
-                description: "leisure suit required. and only fiercest manners"
-            }
-        ];
+        .controller("PartiesListCtrl", function($scope, $meteor) {
+        $scope.parties = $meteor.collection(Parties);
     });
 }
